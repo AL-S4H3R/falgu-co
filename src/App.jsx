8251aa872a5ad479e4234130c400bebdffa551f2
Shlok D. Mange <shlok_mange@protonmail.com>
2021-02-18 23:12:24 +0400

    Logo Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { faEnvelope, faFax, faMobile, faMobileAlt, faSearchLocation } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import wallpaper from './assets/wallpaper_3.jpg'
+import wallpaper from './assets/wallpaper_4.jpg'
+import nta from './assets/nut.png'
 
 const App = () => {
     return (
-        <div className="bg-gray-50 h-screen w-screen">
+        <div className="bg-gray-50 h-screen w-screen overflow-auto">
             <style>
                 @import url('https://fonts.googleapis.com/css2?family=Cookie&display=swap');
             </style>
@@ -73,9 +74,6 @@ const App = () => {
                             <a href="/" className="hover:underline">Brands</a>
                         </li>
                         <li className="text-gray-50 uppercase text-2xl">
-                            <a href="/" className="hover:underline">Login</a>
-                        </li>
-                        <li className="text-gray-50 uppercase text-2xl">
                             <a href="/" className="hover:underline">Get in Touch</a>
                         </li>
                     </ul>
@@ -84,13 +82,28 @@ const App = () => {
             <div 
                 style={{
                     backgroundImage:`url(${wallpaper})`,
+                    opacity:'100%',
+                    height:'75%'
                 }}
-                className="flex items-center justify-center h-screen" 
+                className="flex items-start justify-center h-screen object-contain bg-center" 
             >
-                jcdkjsd
+                <div>
+                    <h2 className="text-5xl font-bold mt-32 text-white">Leaders in trading dried fruits, nuts & seeds.</h2>
+                    <div className="flex justify-center w-full mt-12">
+                        <button className="rounded-full font-medium text-xl p-5 focus:outline-none" style={{backgroundColor:'#f16121',color:'#ffffff'}}>Order Now</button>
+                    </div>
+                </div>
             </div>
-            <footer className="flex justify-center bg-gray-200 text-gray-800 font-bold p-3 text-lg">
-            © 2021 Falgu & Co Limited | Design & Engineering - 4L_SAH3R
+            <div className="flex justify-center">
+                <div>
+                    <h2 className="text-5xl font-semibold text-gray-900 text-center mt-12 font-serif">
+                        We are proud members of
+                    </h2>
+                    <img src={nta} className="mt-24 ml-24 flex justify-center"/>                
+                </div>
+            </div>
+            <footer className="flex justify-center text-gray-900 font-light p-3 text-lg mt-24">
+            © 2021 Falgu & Co Limited | All rights reserved.
             </footer>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom'
 import './index.css'
 import App from './App'
 
-render(<App/>, document.querySelector('#root'))
+const Main = () => {
+    return(
+        <Router>
+            <Switch>
+                <Route component={App} exact={true} path="/"/>
+            </Switch>
+        </Router>
+    )
+}
+
+render(<Main/>, document.querySelector('#root'))
